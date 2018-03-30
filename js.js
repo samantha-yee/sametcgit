c402d4d98edd078664f31ebaed1efdace1a17da9
Sam Yee <samanthayee92@gmail.com>
2018-03-29 20:16:03 -0700

    added under contstruction portfolio

--- js.js
+++ js.js
@@ -124,6 +124,16 @@ if (hideMe == false){
 	}
 })
 
+var header = document.getElementById("header");
+var navs = header.getElementsByClassName("navnav");
+for (var i = 0; i < navs.length; i++) {
+  navs[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
 
 
 
--- js/portjs.js
+++ js/portjs.js
@@ -1,38 +1,77 @@
+var up = document.getElementById('up')
+,	down = document.getElementById('down')
 
-var body = document.getElementById('port-body');
+,	edButt = document.getElementById('edButt')
+,	brandButt = document.getElementById('brandButt')
+,	illButt = document.getElementById('illButt')
+,	filmButt = document.getElementById('filmButt')
 
-var contactButt = document.getElementById('contactButt')
-,	contactme = document.getElementById('contactme');
+,	portTypeTitle = document.getElementById('port-type-title')
 
-contactButt.addEventListener('click', function(){
-	contactme.style.display = 'block';
-	body.style.backgroundColor = '#111111';
+,	portscroll = document.getElementById('port-scroll')
+,	editorial = document.getElementById('editorial')
+,	illustration = document.getElementById('illustration')
+,	branding = document.getElementById('branding')
+,	video = document.getElementById('video')
+;
+
+edButt.addEventListener('click', function(){
+	editorial.style.display = 'block';
+	branding.style.display = 'none';
+	illustration.style.display = 'none';
+	video.style.display = 'none';
 })
 
-var close = document.getElementById('close');
+brandButt.addEventListener('click', function(){
+	branding.style.display = 'block';
+	editorial.style.display = 'none';
+	illustration.style.display = 'none';
+	video.style.display = 'none';
 
-close.addEventListener('click', function(){
-	contactme.style.display = 'none';
-	body.style.backgroundColor = '#F9F9F9';
-});
+})
+
+illButt.addEventListener('click', function(){
+	illustration.style.display = 'block';
+	branding.style.display = 'none';
+	editorial.style.display = 'none';
+	video.style.display = 'none';
+})
+
+filmButt.addEventListener('click', function(){
+	illustration.style.display = 'none';
+	branding.style.display = 'none';
+	editorial.style.display = 'none';
+	video.style.display = 'block';
+})
+
+var portHeader = document.getElementById("port-header");
+var portButts = portHeader.getElementsByClassName("port-butts");
+for (var i = 0; i < portButts.length; i++) {
+  portButts[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("port-active");
+    current[0].className = current[0].className.replace(" port-active", "");
+    this.className += " port-active";
+  });
+}
+
+// var branding = document.getElementById("branding");
+// var sides = branding.getElementsByClassName("sides");
+// for (var i = 0; i < sides.length; i++) {
+//   sides[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("side-active");
+//     current[0].className = current[0].className.replace(" side-active", "");
+//     this.className += " side-active";
+//   });
+// }
 
 
+// var front = document.getElementById('front')
+// ,	back = document.getElementById('back')
+// ,	butzFront = document.getElementById('butz-front')
+// ,	butzBack = document.getElementById('butz-back')
+// ;
 
-// SCROLL
-$('.js-btn').eq(0).click(function () {
-	$('html, body').animate({
-		scrollTop: $('.js-section').eq(0).offset().top
-	}, 300);
-});
+// front.addEventListener('click', function(){
+// 	butzFront.removeClass = 'opaque';
+// })
 
-$('.js-btn').eq(1).click(function () {
-	$('html, body').animate({
-		scrollTop: $('.js-section').eq(1).offset().top
-	}, 300);
-});
-
-$('.js-btn').eq(2).click(function () {
-	$('html, body').animate({
-		scrollTop: $('.js-section').eq(2).offset().top
-	}, 300);
-});
